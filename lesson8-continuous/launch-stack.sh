e21b9713626a68b091763906f041f71c68bc9a91
Paul Duvall <pduvall@gmail.com>
2020-06-04 14:53:43 +0000

    add command line args and variables to allow changes

--- lesson8-continuous/launch-stack.sh
+++ lesson8-continuous/launch-stack.sh
@@ -2,32 +2,41 @@
 # sudo chmod +x *.sh
 # ./launch-stack.sh
 
-sudo rm -rf tmp-gitrepo
-mkdir tmp-gitrepo
-cd tmp-gitrepo
+TMPDIR=${1:-tmp-gitrepo}   
+S3BUCKET=${2:-pmd-security-controls-}
+SAMSTACK=${3:-pmd-security-controls-us-east-1}
+CFNSTACK=${4:-pmd-security-controls}
+PIPELINEYAML=${5:-ceoa-8-pipeline.yml}
+UNENCRYPTEDS3BUCKETPREFIX=${6:-ceoa-8-s3-unencrypted-}
+
+sudo rm -rf $TMPDIR
+mkdir $TMPDIR
+cd $TMPDIR
 git clone https://github.com/PaulDuvall/aws-encryption-workshop.git
 
-aws s3api list-buckets --query 'Buckets[?starts_with(Name, `pmd-security-controls-`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `'$S3BUCKET'`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+
+aws s3 rb s3://$UNENCRYPTEDS3BUCKETPREFIX$(aws sts get-caller-identity --output text --query 'Account') --force
 
 sleep 20
 
-aws cloudformation delete-stack --stack-name pmd-security-controls-us-east-1
+aws cloudformation delete-stack --stack-name $SAMSTACK
 
 sleep 50
 
-aws cloudformation delete-stack --stack-name pmd-security-controls
+aws cloudformation delete-stack --stack-name $CFNSTACK
 
 sleep 25
 
 cd aws-encryption-workshop/lesson8-continuous
 
-aws s3 mb s3://pmd-security-controls-$(aws sts get-caller-identity --output text --query 'Account')
-zip -r pmd-security-controls.zip .
+aws s3 mb s3://$CFNSTACK-$(aws sts get-caller-identity --output text --query 'Account')
+zip -r $CFNSTACK.zip .
 mkdir zipfiles
-cp ceoa-8-pipeline.yml zipfiles
-mv pmd-security-controls.zip zipfiles
+cp $PIPELINEYAML zipfiles
+mv $CFNSTACK.zip zipfiles
 cd zipfiles
 
-aws s3 sync . s3://pmd-security-controls-$(aws sts get-caller-identity --output text --query 'Account')
+aws s3 sync . s3://$CFNSTACK-$(aws sts get-caller-identity --output text --query 'Account')
 
-aws cloudformation create-stack --stack-name pmd-security-controls --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://ceoa-8-pipeline.yml --parameters ParameterKey=EmailAddress,ParameterValue=fake-email@fake-fake-fake-email.com ParameterKey=CodeCommitS3Bucket,ParameterValue=pmd-security-controls-$(aws sts get-caller-identity --output text --query 'Account') ParameterKey=CodeCommitS3Key,ParameterValue=pmd-security-controls.zip ParameterKey=S3ComplianceResourceId,ParameterValue=ceoa-8-s3-unencrypted-$(aws sts get-caller-identity --output text --query 'Account')
+aws cloudformation create-stack --stack-name $CFNSTACK --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://$PIPELINEYAML --parameters ParameterKey=EmailAddress,ParameterValue=fake-email@fake-fake-fake-email.com ParameterKey=CodeCommitS3Bucket,ParameterValue=$CFNSTACK-$(aws sts get-caller-identity --output text --query 'Account') ParameterKey=CodeCommitS3Key,ParameterValue=$CFNSTACK.zip ParameterKey=S3ComplianceResourceId,ParameterValue=ceoa-8-s3-unencrypted-$(aws sts get-caller-identity --output text --query 'Account')
