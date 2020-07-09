e88887e006d5a3c21ece852c86c541645a9e7d11
Paul Duvall <pduvall@gmail.com>
2020-07-09 19:37:56 +0000

    add vars to delete-taskcat

--- delete-taskcat.sh
+++ delete-taskcat.sh
@@ -2,6 +2,10 @@
 # sudo chmod +x *.sh
 # ./delete-taskcat.sh 
 
+PREFIX=tCaT
+# Name used in .taskcat.yml
+PROJECT_NAME=ce
+
 AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
 
 # Generated Code by TaskCat. For example: 7806754vf2r1494aa8b64d1av821418881
@@ -12,38 +16,38 @@ NESTED_APP_CODE=${2:-TBD}
 echo Environment: $TASKCAT
 
 echo "Removing buckets previously used by this script"
-aws s3api list-buckets --query 'Buckets[?starts_with(Name, `tcat-ce`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `$PREFIX-ce`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
 
 echo "Deleting Lesson 8 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l8-$TASKCAT-$AWS_REGION
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l8-$TASKCAT-$AWS_REGION
-aws cloudformation delete-stack --stack-name tCaT-ce-l8-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l8-$TASKCAT-$AWS_REGION
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l8-$TASKCAT-$AWS_REGION
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l8-$TASKCAT
 
 echo "Deleting Lesson 5 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-ddb-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-ebs-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-kms-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-s3-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l5-rest-ddb-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l5-rest-ebs-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l5-rest-kms-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l5-rest-s3-$TASKCAT
 
 echo "Deleting Lesson 4 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT-cloudfront
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l4-$TASKCAT-cloudfront
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT-acm
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l4-$TASKCAT-acm
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l4-$TASKCAT-cloudfront
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l4-$TASKCAT-cloudfront
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l4-$TASKCAT-acm
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l4-$TASKCAT-acm
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l4-$TASKCAT
 
 echo "Deleting Lesson 3 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-rds
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-rds
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-vpc
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-vpc
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-rds
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-rds
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-vpc
+aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT-$AWS_REGION-vpc
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-l3-$TASKCAT
 
 echo "Deleting Lesson 1 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-pipeline-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-automate-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-pipeline-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-lesson1-pipeline-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-lesson1-automate-$TASKCAT
+aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-lesson1-pipeline-$TASKCAT
