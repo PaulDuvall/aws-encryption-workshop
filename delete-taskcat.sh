f92438d07f94eb17ae09f12663757e5ad3f34e76
Paul Duvall <pduvall@gmail.com>
2020-07-10 16:57:13 +0000

    ensure tcat-ce s3 buckets are deleted

--- delete-taskcat.sh
+++ delete-taskcat.sh
@@ -3,6 +3,7 @@
 # ./delete-TASKCAT_CODE.sh 
 
 PREFIX=tCaT
+S3_PREFIX=tcat
 # Name used in .taskcat.yml
 PROJECT_NAME=ce
 
@@ -16,7 +17,7 @@ NESTED_APP_CODE=${2:-TBD}
 echo Environment: $TASKCAT_CODE
 
 echo "Removing buckets previously used by this script"
-aws s3api list-buckets --query 'Buckets[?starts_with(Name, `'$PREFIX-$PROJECT_NAME'`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `'$S3_PREFIX-$PROJECT_NAME'`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
 
 # Lesson 8
 echo "Deleting $PREFIX-$PROJECT_NAME-l8-$TASKCAT_CODE-$AWS_REGION stack"
